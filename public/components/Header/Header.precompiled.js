f42feea6e690e9150637757fb16e84f23add3258
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-09-26 10:34:07 +0300

    поменял расположение иконки поиска

--- public/components/Header/Header.precompiled.js
+++ public/components/Header/Header.precompiled.js
@@ -10,11 +10,11 @@ templates['Header.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(co
 
   return "<div class=\"header\" data-section=\""
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"key") : depth0), depth0))
-    + "\">\r\n    <div class=\"header-search\">\r\n        <input type=\"text\" class=\"header-search-input\" placeholder=\""
-    + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"search") : depth0)) != null ? lookupProperty(stack1,"placeholder") : stack1), depth0))
-    + "\">\r\n        <img src=\""
+    + "\">\r\n    <div class=\"header-search\">\r\n        <img src=\""
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"search") : depth0)) != null ? lookupProperty(stack1,"img") : stack1), depth0))
-    + "\" alt=\"поиск\">\r\n    </div>\r\n    <div class=\"header-profile\">\r\n        <img src=\""
+    + "\" alt=\"поиск\">\r\n        <input type=\"text\" class=\"header-search-input\" placeholder=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"search") : depth0)) != null ? lookupProperty(stack1,"placeholder") : stack1), depth0))
+    + "\">\r\n    </div>\r\n    <div class=\"header-profile\">\r\n        <img src=\""
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"profile") : depth0)) != null ? lookupProperty(stack1,"logoutImg") : stack1), depth0))
     + "\" alt=\"Выйти\" class=\"header-profile-logout\">\r\n        <img src=\""
     + alias2(alias1(((stack1 = (depth0 != null ? lookupProperty(depth0,"profile") : depth0)) != null ? lookupProperty(stack1,"avatar") : stack1), depth0))
