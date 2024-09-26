a03424403f46d3ac2642270878cc9795925a4327
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-09-26 15:04:37 +0300

    тестовый сервер для авторизации

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const ip = 'http://127.0.0.1';
 const port = 8000;
@@ -13,14 +15,80 @@ const posts = [
 	},
 ];
 
+const users = [
+	{
+		id: 1,
+		sessionId: null,
+		email: 'test@mail.com',
+		password: 'password',
+		firstName: 'firstName',
+		lastName: 'lastName',
+	},
+];
+
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public', { fallthrough: true }));
 app.use(express.static('./node_modules'));
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
 	next();
+});
+
+app.post('/auth/login', (req, res) => {
+	console.log('login');
+	const body = req.body;
+	console.log('body:', body);
+	let sessionId;
+	users.forEach((user) => {
+		if (body.email === user.email && body.password === user.password) {
+			user.sessionId = user.id;
+			sessionId = user.sessionId;
+			console.log('user:', user);
+		}
+	});
+	if (sessionId) {
+		res.status(200);
+		res.cookie('sessionid', sessionId, { maxAge: '3600000' });
+	} else {
+		res.status(401);
+	}
+	res.send();
+});
+
+app.post('/auth/signup', (req, res) => {
+	console.log('signup');
+	const body = req.body;
+	const user = {
+		id: users.length + 1,
+		sessionId: users.length + 1,
+		email: body.email,
+		password: body.password,
+		firstName: body.firstName,
+		lastName: body.lastName,
+	};
+	users.push(user);
+	console.log('users:', users);
+	res.cookie('sessionid', user.sessionId, { maxAge: 86400000 });
+	res.send();
+});
+
+app.post('/auth/logout', (req, res) => {
+	console.log('logout');
+	console.log('cookies:', req.cookies);
+	let sessionId = req.cookies['sessionid'];
+	users.forEach((user) => {
+		if (sessionId == user.sessionId) {
+			user.sessionId = null;
+			console.log('user:', user);
+		}
+	});
+	res.cookie('sessoinid', 0, { maxAge: -1 });
+	res.send();
 });
 
 let counter = 0;
@@ -43,6 +111,7 @@ app.get('/public/img/', (req, res) => {
 
 app.get('*', (req, res) => {
 	console.log('Запрос:', req.originalUrl);
+	console.log('Куки:', req.cookies);
 	res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
