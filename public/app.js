6a53b297be30e01ff5729f55a2b070ca4850bb9a
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-09-30 12:54:04 +0300

    add logs

--- public/app.js
+++ public/app.js
@@ -274,6 +274,7 @@ export default class App {
 							this.goToPage(PAGE_LINKS.feed, true);
 						} else {
 							const data = response.json();
+							console.log(data.message);
 							if (data.message === 'user already exists') {
 								signUp.printError(
 									'Пользователь в таким email уже существует',
@@ -333,6 +334,7 @@ export default class App {
 							this.goToPage(PAGE_LINKS.feed, true);
 						} else {
 							const data = response.json();
+							console.log(data.message);
 							if (data.message === 'wrong email or password') {
 								login.printError('Неверная почта или пароль');
 							} else {
