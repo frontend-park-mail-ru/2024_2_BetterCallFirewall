a6ff95b57d2974e2725fa7d79b237a9e8d70d386
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-03 13:01:33 +0300

    fix: change handle

--- public/views/profileEdit/viewProfileEdit.ts
+++ public/views/profileEdit/viewProfileEdit.ts
@@ -1,63 +1,68 @@
 import { ActionUpdateProfileEdit } from '../../actions/actionProfileEdit';
 import { Root } from '../../components';
-import { ProfileEditForm, IProfileEditFormConfig } from '../../components/ProfileEditForm/ProfileEditForm';
+import {
+	ProfileEditForm,
+	IProfileEditFormConfig,
+} from '../../components/ProfileEditForm/ProfileEditForm';
 import dispatcher from '../../dispatcher/dispatcher';
 import { ChangeProfileEdit } from '../../stores/storeProfileEditForm';
-import { ComponentsHome, HomeConfig, IViewHome, ViewHome } from '../home/viewHome';
+import {
+	ComponentsHome,
+	HomeConfig,
+	IViewHome,
+	ViewHome,
+} from '../home/viewHome';
 
 export type ComponentsProfileEdit = {
-    profileEditForm?: ProfileEditForm;
+	profileEditForm?: ProfileEditForm;
 } & ComponentsHome;
 
 export interface ViewProfileEditConfig extends HomeConfig {
-    profileEditForm: IProfileEditFormConfig;
+	profileEditForm: IProfileEditFormConfig;
 }
 
 export interface IViewProfileEdit extends IViewHome {
-    handleChange(change: ChangeProfileEdit): void;
+	handleChange(change: ChangeProfileEdit): void;
 }
 
 export class ViewProfileEdit extends ViewHome implements IViewProfileEdit {
-    protected _configProfileEdit: ViewProfileEditConfig;
-    protected _components: ComponentsProfileEdit = {};
+	protected _configProfileEdit: ViewProfileEditConfig;
+	protected _components: ComponentsProfileEdit = {};
 
-    constructor(config: ViewProfileEditConfig, root: Root) {
-        super(config, root);
-        this._configProfileEdit = config;
-    }
+	constructor(config: ViewProfileEditConfig, root: Root) {
+		super(config, root);
+		this._configProfileEdit = config;
+	}
 
-    handleChange(change: ChangeProfileEdit): void {
-        super.handleChange(change);
-        switch (change.type) {
-            default:
-                this.updateViewProfileEdit(change.data);
-        }
-    }
+	handleChange(change: ChangeProfileEdit): void {
+		super.handleChange(change);
+	}
 
-    render(): void {
-        this._render();
-        dispatcher.getAction(
-            new ActionUpdateProfileEdit(this._configProfileEdit.profileEditForm),
-        );
-    }
+	render(): void {
+		this._render();
+		dispatcher.getAction(new ActionUpdateProfileEdit());
+	}
 
-    updateViewProfileEdit(data: ViewProfileEditConfig): void {
-        this._configProfileEdit = data;
-        this._render();
-    }
+	updateViewProfileEdit(data: ViewProfileEditConfig): void {
+		this._configProfileEdit = data;
+		this._render();
+	}
 
-    protected _render(): void {
-        super._render();
-        this._renderProfileEditForm();
-    }
+	protected _render(): void {
+		super._render();
+		this._renderProfileEditForm();
+	}
 
-    protected _renderProfileEditForm(): void {
-        const content = this._components.content;
-        if (!content) {
-            throw new Error('content does no exist on ViewProfileEdit');
-        }
-        const profileEditForm = new ProfileEditForm(this._configProfileEdit.profileEditForm, content);
-        profileEditForm.render();
-        this._components.profileEditForm = profileEditForm;
-    }
+	protected _renderProfileEditForm(): void {
+		const content = this._components.content;
+		if (!content) {
+			throw new Error('content does no exist on ViewProfileEdit');
+		}
+		const profileEditForm = new ProfileEditForm(
+			this._configProfileEdit.profileEditForm,
+			content,
+		);
+		profileEditForm.render();
+		this._components.profileEditForm = profileEditForm;
+	}
 }
