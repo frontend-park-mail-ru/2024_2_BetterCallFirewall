a7283942c42320439dcc83a2b3e677427227883b
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-03 17:57:05 +0300

    feat: add prefix to img

--- public/models/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-	[key: string]: string; // Индексная сигнатура, чтобы IDE знал, что это Record<string, string>
-	email: string;
-	password: string;
-	firstName: string;
-	lastName: string;
-}
---
+++ public/modules/parseImage.ts
@@ -0,0 +1,7 @@
+import { ROOT } from '../config';
+
+const parseImage = (img: string): string => {
+	return ROOT + img;
+};
+
+export default parseImage;
