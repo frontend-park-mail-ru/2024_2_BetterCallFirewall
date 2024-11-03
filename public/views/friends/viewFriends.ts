4b58c4b5e3cb113f97026a1be0712ca3cbf03f47
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-03 12:10:55 +0300

    feat: friends component

--- public/views/friends/viewFriends.ts
+++ public/views/friends/viewFriends.ts
@@ -1,17 +1,23 @@
 import { Root } from '../../components';
 import { Friend } from '../../components/Friend/Friend';
+import { Friends, FriendsConfig } from '../../components/Friends/Friends';
 import { ChangeFriends } from '../../stores/storeFriends';
 import { HomeConfig, IViewHome, ViewHome } from '../home/viewHome';
 
-export interface ViewFriendsConfig extends HomeConfig {}
+export interface ViewFriendsConfig extends HomeConfig {
+	friends: FriendsConfig;
+}
 
 export interface IViewFriends extends IViewHome {
 	handleChange(change: ChangeFriends): void;
 }
 
 export class ViewFriends extends ViewHome implements IViewFriends {
+	protected _configFriends: ViewFriendsConfig;
+
 	constructor(config: ViewFriendsConfig, root: Root) {
 		super(config, root);
+		this._configFriends = config;
 	}
 
 	handleChange(change: ChangeFriends): void {
@@ -21,12 +27,20 @@ export class ViewFriends extends ViewHome implements IViewFriends {
 		}
 	}
 
-	protected _renderContent(): void {
-		super._renderContent();
-		const content = this._components.content;
-		if (!content) {
-			throw new Error('content does no exist on ViewProfile');
-		}
+	render(): void {
+		this._render();
+	}
+
+	protected _render(): void {
+		super._render();
+		this._renderFriends();
+	}
+
+	protected _renderFriends(): void {
+		const content = this.content;
+		const friends = new Friends(this._configFriends.friends, content);
+		friends.render();
+
 		// Тестовые друзья
 		for (let i = 0; i < 10; i++) {
 			const friend = new Friend(
