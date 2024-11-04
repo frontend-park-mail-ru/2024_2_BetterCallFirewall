9e7ebfb2abddf316b4fcc8e3b70dcb507e7b2c2a
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-11-04 23:58:57 +0300

    deleted unknown

--- public/components/Friend/Friend.ts
+++ public/components/Friend/Friend.ts
@@ -16,7 +16,6 @@ export interface IFriend extends BaseComponent {}
 
 export class Friend extends BaseComponent implements IFriend {
 	protected _config: IFriendConfig | null;
-	protected isUnknown: boolean = false;
 	/**
 	 * Instance of friend
 	 *
@@ -26,9 +25,6 @@ export class Friend extends BaseComponent implements IFriend {
 	constructor(config: IFriendConfig, parent: IBaseComponent) {
 		super(config, parent);
 		this._config = config;
-		if (!this._config.isFriend && !this._config.isSubscriber && !this._config.isSubscription) {
-			this.isUnknown = true;
-		}
 	}
 
 	render(show: boolean = true): string {
@@ -40,7 +36,6 @@ export class Friend extends BaseComponent implements IFriend {
 		super._prerender();
 		this._templateContext = { 
 			...this.config,
-			isUnknown: this.isUnknown,
 		 };
 	}
 
