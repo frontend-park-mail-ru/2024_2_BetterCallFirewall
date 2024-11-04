8d5eb2bb4edac853c7666014ba61ad84a2769061
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-04 16:18:46 +0300

    fix: update view path

--- public/reducers/reducerProfile.ts
+++ public/reducers/reducerProfile.ts
@@ -1,6 +1,7 @@
 import { Action } from '../actions/action';
 import {
 	ACTION_PROFILE_TYPES,
+	ActionProfileGetHeaderSuccessData,
 	ActionProfileGetYourOwnProfileSuccessData,
 	ActionProfileRequestSuccessData,
 	ActionUpdateProfileData,
@@ -23,6 +24,11 @@ export const reducerProfile = (
 	const newState = deepClone(state);
 	let actionData;
 	switch (action?.type) {
+		case ACTION_PROFILE_TYPES.getHeaderSuccess: {
+			const actionData = action.data as ActionProfileGetHeaderSuccessData;
+			newState.path = `/${actionData.headerResponse.author_id}`;
+			return newState;
+		}
 		case ACTION_PROFILE_EDIT_TYPES.requestSuccess: {
 			actionData = action.data as ActionProfileEditRequestSuccessData;
 			const profileConfig = toProfileConfig(
