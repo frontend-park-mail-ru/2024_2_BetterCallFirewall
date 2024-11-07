dc0cbb23e6726d2c79753af161c0641d964925fd
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-07 20:33:32 +0300

    fix: concat new profiles

--- public/reducers/reducerFriends.ts
+++ public/reducers/reducerFriends.ts
@@ -24,11 +24,18 @@ export const reducerFriends = (
 	switch (action.type) {
 		case ACTION_FRIENDS_TYPES.getUsersSuccess: {
 			const actionData = action.data as ActionProfileGetUsersSuccessData;
-			newState.users.friendsConfig = actionData.users
-				.filter(user => !user.is_subscriber && !user.is_friend && !user.is_subscription)
-				.map((user) => {
-					return toFriendConfig(user);
-			});
+			newState.users.friendsConfig = newState.users.friendsConfig.concat(
+				actionData.users
+					.filter(
+						(user) =>
+							!user.is_subscriber &&
+							!user.is_friend &&
+							!user.is_subscription,
+					)
+					.map((user) => {
+						return toFriendConfig(user);
+					}),
+			);
 			return newState;
 		}
 		case ACTION_FRIENDS_TYPES.getSubscribersSuccess: {
@@ -50,8 +57,8 @@ export const reducerFriends = (
 		case ACTION_FRIENDS_TYPES.getSubscriptionsSuccess: {
 			const actionData =
 				action.data as ActionProfileGetSubscriptionsSuccessData;
-			newState.subscriptions.friendsConfig = actionData.subscriptions.map((subscription) =>
-				toFriendConfig(subscription),
+			newState.subscriptions.friendsConfig = actionData.subscriptions.map(
+				(subscription) => toFriendConfig(subscription),
 			);
 			return newState;
 		}
