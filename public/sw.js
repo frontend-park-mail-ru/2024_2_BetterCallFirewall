40a5619f6aff51bfec31860f8b96376b77980e16
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-07 16:55:12 +0300

    feat: add SW

---
+++ public/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'Vilka';
+const urlsCache = ['/'];
+
+// Добавляем урлы в кеш
+this.addEventListener('install', (event) => {
+	event.waitUntil(CACHE_NAME).then((cache) => {
+		console.log('Кэшируем');
+		return cache.addAll(urlsCache);
+	});
+});
+
+// Отработка запросов
+this.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			// Ищем в кеше
+			console.log('Идем в кеш');
+			if (response) {
+				return response;
+			}
+			// Идем в сеть
+			console.log('Идем в сеть');
+			return fetch(event.request).then((networkResponse) => {
+				if (!networkResponse || networkResponse.status !== 200) {
+					console.log('Пришло что-то нехорошее');
+					return networkResponse;
+				}
+				// Сохраняем
+				console.log('Сохраняем');
+				return caches.open(CACHE_NAME).then((cache) => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			});
+		}),
+	);
+});
+
+// Удаляем старые кеши
+this.addEventListener('activate', (event) => {
+	console.log('Удаляем кеши');
+	const cacheWhiteList = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheWhiteList.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				}),
+			);
+		}),
+	);
+});
