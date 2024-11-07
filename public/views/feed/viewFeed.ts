9ee1203d26d1a5f27a6170ee025f6b8199a8e1c9
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-08 02:06:37 +0300

    fix: go to profile on post create

--- public/views/feed/viewFeed.ts
+++ public/views/feed/viewFeed.ts
@@ -5,7 +5,6 @@ import { ActionPostEditGoTo } from '../../actions/actionPostEdit';
 import { ACTION_SIGNUP_TYPES } from '../../actions/actionSignup';
 import api from '../../api/api';
 import { IPostConfig, Post, Root } from '../../components';
-import { PAGE_LINKS } from '../../config';
 import { ChangeFeed } from '../../stores/storeFeed';
 import { HomeConfig, IViewHome, ViewHome } from '../home/viewHome';
 
@@ -29,7 +28,7 @@ export class ViewFeed extends ViewHome implements IViewFeed {
 		switch (change.type) {
 			case ACTION_FEED_TYPES.postCreateSuccess:
 				this.sendAction(
-					new ActionMenuLinkClick({ href: PAGE_LINKS.feed }),
+					new ActionMenuLinkClick({ href: this._profileLinkHref }),
 				);
 				break;
 			case ACTION_LOGIN_TYPES.loginClickSuccess:
