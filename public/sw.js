df7042068ef84b0e75e61062dee41b21e4cb4c9f
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-11-08 16:30:11 +0300

    deleted logs

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,22 @@
 const CACHE_NAME = 'Vilka-cache-v5';
 const urlsCache = ['/'];
 
-// Добавляем урлы в кеш
 this.addEventListener('install', (event) => {
 	event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-		console.log('Кэшируем');
 		return cache.addAll(urlsCache);
 	}));
 });
 
-// Отработка запросов
 this.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			// Ищем в кеше
-			console.log('Идем в кеш');
 			if (response) {
 				return response;
 			}
-			// Идем в сеть
-			console.log('Идем в сеть');
 			return fetch(event.request).then((networkResponse) => {
 				if (!networkResponse || networkResponse.status !== 200) {
-					console.log('Пришло что-то нехорошее');
 					return networkResponse;
 				}
-				// Сохраняем
-				console.log('Сохраняем');
 				return caches.open(CACHE_NAME).then((cache) => {
 					cache.put(event.request, networkResponse.clone());
 					return networkResponse;
@@ -36,9 +26,7 @@ this.addEventListener('fetch', (event) => {
 	);
 });
 
-// Удаляем старые кеши
 this.addEventListener('activate', (event) => {
-	console.log('Удаляем кеши');
 	const cacheWhiteList = [CACHE_NAME];
 	event.waitUntil(
 		caches.keys().then((cacheNames) => {
