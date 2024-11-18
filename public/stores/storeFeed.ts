ce206bbb5fe84f4894726040612b574e609b19d1
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-18 15:51:37 +0300

    fix: vdom

--- public/stores/storeFeed.ts
+++ public/stores/storeFeed.ts
@@ -21,7 +21,9 @@ export class StoreFeed extends BaseStore implements Store {
 
 	handleAction(action: Action): void {
 		this._state = { ...this._state, ...this._storeHome.state };
+		console.log('storeFeed: state before:', this._state.posts);
 		this._state = reducerFeed(this._state, action);
+		console.log('storeFeed: state after:', this._state.posts);
 		if (this._registeredView?.active) {
 			this._registeredView.handleChange({
 				type: action.type,
