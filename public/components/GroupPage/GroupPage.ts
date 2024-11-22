dbc227206afca36504852eb9261fad6c5a24f1ac
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-11-22 18:30:47 +0300

    dev merge

---
+++ public/components/GroupPage/GroupPage.ts
@@ -0,0 +1,69 @@
+import {
+	findVNodeByKey,
+	VNode,
+} from '../../modules/vdom';
+import Component, { ComponentConfig } from '../Component';
+import { PostConfig, Post } from '../Post/Post';
+
+type Posts = Post[];
+
+export interface GroupPageConfig extends ComponentConfig {
+	id: number;
+	name?: string;
+	description?: string;
+	img: string;
+	posts: PostConfig[];
+	createPostHref: string;
+	isAuthor: boolean;
+}
+
+export class GroupPage extends Component {
+	protected override _config: GroupPageConfig;
+	private _posts: Posts = [];
+
+	constructor(config: GroupPageConfig, parent: Component) {
+		super(config, parent);
+		this._config = config;
+	}
+
+	get config(): GroupPageConfig {
+		if (this._config) {
+			return this._config;
+		}
+		throw new Error('config not found');
+	}
+
+	set config(config: GroupPageConfig) {
+		this._config = config;
+	}
+
+	get createPostLinkVNode(): VNode {
+		const vnode = findVNodeByKey(this.vnode, 'createPost');
+		if (!vnode) {
+			throw new Error('createPostLink vnode not found');
+		}
+		return vnode;
+	}
+
+	get posts(): Post[] {
+		return this._posts;
+	}
+
+	render(): string {
+		this._prerender();
+		return this._render('GroupPage.hbs');
+	}
+
+	protected _prerender(): void {
+		super._prerender();
+		this._posts = this._config.posts.map((config) => {
+			return new Post(config, this);
+		});
+		this._templateContext = {
+			...this._templateContext,
+			posts: this._posts.map((post) => {
+				return post.render();
+			}),
+		};
+	}
+}
