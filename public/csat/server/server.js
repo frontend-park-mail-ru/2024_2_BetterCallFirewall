64cbf4e2202bf64a6a21c26838b6b2aff38a9cff
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-11-23 15:21:18 +0300

    fix: csat

--- public/csat/server/server.js
+++ public/csat/server/server.js
@@ -1,16 +1,24 @@
 express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 const port = 8001;
 
 const app = express();
 
-app.use(cookieParser());
-app.use(express.json({ strict: false }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public', { fallthrough: true }));
-app.use(express.static('./node_modules'));
+app.use(express.static('../../', { fallthrough: true }));
+
+app.get('/handlebars.runtime.js', (req, res) => {
+	res.sendFile(
+		path.join(
+			__dirname,
+			'../node_modules/handlebars/dist/handlebars.runtime.js',
+		),
+	);
+});
+
+app.get('/precompiled.js', (req, res) => {
+	res.sendFile(path.join(__dirname, '../precompiled.js'));
+});
 
 app.get('/dist/bundle.js', (req, res) => {
 	res.sendFile(path.join(__dirname, '../dist', 'bundle.js'));
