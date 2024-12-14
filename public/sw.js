ca593ce17c6ef16de65df41214ceb8191edc721f
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-14 11:20:47 +0300

    fix: network first

--- public/sw.js
+++ public/sw.js
@@ -2,25 +2,35 @@ const CACHE_NAME = 'Vilka-cache-v5';
 const urlsCache = ['/'];
 
 this.addEventListener('install', (event) => {
-	event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-		return cache.addAll(urlsCache);
-	}));
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsCache);
+		}),
+	);
 });
 
 this.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			if (response) {
-				return response;
-			}
+			// if (response) {
+			// 	return response;
+			// }
 			return fetch(event.request).then((networkResponse) => {
-				if (!networkResponse || networkResponse.status !== 200) {
-					return networkResponse;
+				// if (!networkResponse || networkResponse.status !== 200) {
+				// 	return networkResponse;
+				// }
+				if (networkResponse) {
+					return caches.open(CACHE_NAME).then((cache) => {
+						cache.put(event.request, networkResponse.clone());
+						return networkResponse;
+					});
+				} else {
+					return response;
 				}
-				return caches.open(CACHE_NAME).then((cache) => {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				});
+				// return caches.open(CACHE_NAME).then((cache) => {
+				// 	cache.put(event.request, networkResponse.clone());
+				// 	return networkResponse;
+				// });
 			});
 		}),
 	);
