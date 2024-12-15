c02c406af2fa9d70a1f97e7863a1a476e7428dcc
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-12-15 22:35:52 +0300

    sticker form handler

--- public/components/Stickers/Stickers.ts
+++ public/components/Stickers/Stickers.ts
@@ -33,6 +33,10 @@ export class Stickers extends Component {
 		return this._stickerCreateForm;
 	}
 
+	get stickerCreateFormVNode(): VNode {
+		return this._findVNodeByKey('FORM-form-3');
+	}
+
 	get fileInputVNode(): VNode {
 		return this._findVNodeByKey('LABEL-form__upload-1');
 	}
