214b8088340de2de44075a124ac65951f77e5731
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-16 12:47:41 +0300

    fix: send sticker on chat

--- public/models/sticker.ts
+++ public/models/sticker.ts
@@ -1,15 +1,13 @@
 import { StickerConfig } from '../components/Sticker/Sticker';
 
-export interface StickerResponse {
-	file: string;
-}
+export type StickerResponse = string;
 
 export const toStickerConfig = (
 	stickerResponse: StickerResponse,
 ): StickerConfig => {
 	const stickerData: StickerConfig = {
 		key: `sticker-${stickerResponse}`,
-		file: stickerResponse.file,
+		file: stickerResponse,
 	};
 
 	return stickerData;
