bc037c215aafb29c7ee1258b684a178715a69a68
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-16 03:32:09 +0300

    test

--- public/config.ts
+++ public/config.ts
@@ -24,6 +24,9 @@ import { ViewGroupEditConfig } from './views/groupEdit/viewGroupEdit';
 import { ViewStickersConfig } from './views/stickers/viewStickers';
 import { ICreateStickerFormConfig } from './components/CreateStickerForm/CreateStickerForm';
 
+// eslint-disable-next-line
+import Component from './components/Component';
+
 const DEBUG: boolean = false;
 
 export const ROOT: string = DEBUG
