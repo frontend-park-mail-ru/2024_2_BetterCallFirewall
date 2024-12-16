8ce752ac70ed7dd8fe5d0b0ca8267232dbc269b9
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-16 17:00:43 +0300

    fix: webpack build optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 	entry: './public/index.ts',
@@ -13,14 +16,43 @@ module.exports = {
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65,
+							},
+							pngquant: {
+								quality: [0.65, 0.9],
+								speed: 4,
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							webp: {
+								quality: 75,
+							},
+						},
+					},
+					'file-loader',
+				],
+			},
 		],
 	},
 	resolve: {
 		extensions: ['.ts', '.js', '.scss'],
 	},
-	mode: 'development',
+	mode: 'production',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'public'),
 	},
+	optimization: {
+		minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
+	},
+	plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
 };
