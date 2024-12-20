72b80435b3b7ea3303a0753df683adeb3ccd7a87
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-20 17:58:31 +0300

    fix: post expand button exception

--- public/components/Post/Post.ts
+++ public/components/Post/Post.ts
@@ -345,8 +345,15 @@ export class Post extends Component {
 		this.lastAttachmentImageVNode?.handlers.push({
 			event: 'load',
 			callback: () => {
-				(this.expandButtonVNode.element as HTMLElement).style.display =
-					this._isContentHeightBig() ? 'block' : 'none';
+				try {
+					(
+						this.expandButtonVNode.element as HTMLElement
+					).style.display = this._isContentHeightBig()
+						? 'block'
+						: 'none';
+				} catch {
+					return;
+				}
 			},
 		});
 	}
