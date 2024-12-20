2bb569633dbe04318f9356f9ae4a2529ddf18938
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2024-12-20 14:49:23 +0300

    fix: remove DOMContentLoaded event

--- public/views/feed/viewFeed.ts
+++ public/views/feed/viewFeed.ts
@@ -84,12 +84,6 @@ export class ViewFeed extends ViewHome {
 		super._addHandlers();
 		this._addScrollHandler();
 		this._addPostsHandler();
-		this._root.addDocumentHandler({
-			event: 'DOMContentLoaded',
-			callback: () => {
-				alert('DOMContentLoaded');
-			},
-		});
 	}
 
 	private get lastPostId(): number | undefined {
